e7083879c67ceb1db83a46f630ae276e52818d3c
SH <shhh1006@daum.net>
2022-10-28 00:46:35 +0900

    :sparkles: feat : Build modal at center button - 중앙 버튼 누르면 모달창이 뜨게끔은 - 탭 부분을 누르면 모달이 안열리는 것 수정해야함 - 버튼을 누르면 모달이 닫히게끔 해야함

---
+++ FrontEnd/Gimochi/src/components/HomeModal.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
+
+function HomeModal({ navigation }) {
+  return (
+    <View style={{ flex: 1 }}>
+      <Pressable
+        style={[StyleSheet.absoluteFill, { backgroundColor: 'rgba(0, 0, 0, 0.3)' }]}
+        onPress={navigation.goBack}
+      />
+      <View
+        style={{ width: '100%', height: '30%', position: 'absolute', bottom: 0, backgroundColor: 'white' }}
+      >
+        <Text style={{ textAlign: 'center' }}>Create Posts !! This is Modal</Text>
+      </View>
+    </View>
+  );
+}
+
+export default HomeModal;
