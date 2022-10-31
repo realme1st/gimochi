082b43d9b5cf4d6fbe6b267626300ea95978d3ad
김다은 <vkfksqlcgkxm@naver.com>
2022-10-31 13:11:42 +0900

    :sparkles: Session Controller 기능 추가 (R,D) 세션 수정, 세션 삭제 기능 구현

--- backend/src/main/java/com/ssafy/api/controller/SessionController.java
+++ backend/src/main/java/com/ssafy/api/controller/SessionController.java
@@ -20,19 +20,6 @@ public class SessionController {
     @Autowired
     SessionService sessionService;
 
-    @PostMapping()
-    @ApiOperation(value = "세션 생성", notes = "<strong>세션 정보를 입력하여</strong> 세션을 만든다.")
-    @ApiResponses({
-            @ApiResponse(code = 200, message = "성공"),
-            @ApiResponse(code = 401, message = "권한 없음"),
-            @ApiResponse(code = 400, message = "잘못된 요청"),
-            @ApiResponse(code = 404, message = "해당하는 챌린지 없음"),
-            @ApiResponse(code = 500, message = "서버 오류")
-    })
-    public ResponseEntity<? extends BasicResponse> createSession (@RequestBody SessionReqDto sessionReqDto){
-        return ResponseEntity.ok().body(new CommonResponseEntity<>(sessionService.createSession(sessionReqDto)));
-    }
-
     @GetMapping("/type")
     @ApiOperation(value = "세션 타입 조회", notes = "<strong>세션 타입을 조회한다.</strong>")
     @ApiResponses({
@@ -42,8 +29,47 @@ public class SessionController {
             @ApiResponse(code = 404, message = "해당하는 정보 없음"),
             @ApiResponse(code = 500, message = "서버 오류")
     })
-    public ResponseEntity<? extends BasicResponse> getSessionTypeList(){
+    public ResponseEntity<? extends BasicResponse> getSessionTypeList() {
         return ResponseEntity.ok().body(new CommonResponseEntity<>(sessionService.getSessionTypeList()));
     }
 
+    @PostMapping()
+    @ApiOperation(value = "세션 생성", notes = "<strong>세션 정보를 입력하여</strong> 세션을 만든다.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공"),
+            @ApiResponse(code = 401, message = "권한 없음"),
+            @ApiResponse(code = 400, message = "잘못된 요청"),
+            @ApiResponse(code = 404, message = "해당하는 챌린지 없음"),
+            @ApiResponse(code = 500, message = "서버 오류")
+    })
+    public ResponseEntity<? extends BasicResponse> createSession(@RequestBody SessionReqDto sessionReqDto) {
+        return ResponseEntity.ok().body(new CommonResponseEntity<>(sessionService.createSession(sessionReqDto)));
+    }
+
+
+    @GetMapping("/session/{sessionId}")
+    @ApiOperation(value = "세션 조회", notes = "<strong>세션 id를 입력하여</strong> 세션을 조회한다.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공"),
+            @ApiResponse(code = 401, message = "권한 없음"),
+            @ApiResponse(code = 400, message = "잘못된 요청"),
+            @ApiResponse(code = 404, message = "해당하는 정보 없음"),
+            @ApiResponse(code = 500, message = "서버 오류")
+    })
+    public ResponseEntity<? extends BasicResponse> getSession(@PathVariable Long sessionId) {
+        return ResponseEntity.ok().body(new CommonResponseEntity<>(sessionService.getSession(sessionId)));
+    }
+
+    @DeleteMapping("/session/{sessionId}")
+    @ApiOperation(value = "세션 삭제", notes = "<strong>세션 id를 입력하여</strong> 세션을 삭제한다.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공"),
+            @ApiResponse(code = 401, message = "권한 없음"),
+            @ApiResponse(code = 400, message = "잘못된 요청"),
+            @ApiResponse(code = 404, message = "해당하는 정보 없음"),
+            @ApiResponse(code = 500, message = "서버 오류")
+    })
+    public ResponseEntity<? extends BasicResponse> deleteSession(@PathVariable Long sessionId) {
+        return ResponseEntity.ok().body(new CommonResponseEntity<>(sessionService.deleteSession(sessionId)));
+    }
 }
