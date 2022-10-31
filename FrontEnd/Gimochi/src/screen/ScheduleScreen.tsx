f9f14024cb89c78752bdffedb907eb2f2cebb78a
SH <shhh1006@daum.net>
2022-10-31 16:04:08 +0900

    :sparkles: feat : Build Calendars - 홈 화면, 출석체크 화면, 일정관리 화면에 캘린더 적용 - props로 marking data들 보내야

--- FrontEnd/Gimochi/src/screen/ScheduleScreen.tsx
+++ FrontEnd/Gimochi/src/screen/ScheduleScreen.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import Calendars from '../components/Calendar';
 
 function ScheduleScreen() {
   return (
     <View>
       <Text>일정관리</Text>
+      <Calendars />
     </View>
   );
 }
