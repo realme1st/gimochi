ab07bd40517221a049f2a2bde20236707b873fe6
전민재 <minjae77777@naver.com>
2022-10-31 14:03:29 +0900

    :sparkles: feat : login test

--- FrontEnd/Gimochi/src/screen/MypageScreen.tsx
+++ FrontEnd/Gimochi/src/screen/MypageScreen.tsx
@@ -1,12 +1,60 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import axios from 'axios';
+import { Text, View, LogBox, TouchableOpacity } from 'react-native';
+import { Linking } from 'react-native';
+import { WebView } from 'react-native-webview';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-function MypageScreen() {
+LogBox.ignoreLogs(['Remote debugger']);
+
+const runFirst = 'window.ReactNativeWebView.postMessage("this is message from web");';
+
+const MypageScreen = ({ navigation: { navigate } }) => {
+  // const REST_API_KEY = process.env.REACT_APP_REST_API_KEY;
+  const REST_API_KEY = '0e3c9cecfd800e2aae8228d69a635959';
+  const REDIRECT_URI = 'http://localhost:8081/kakao/oauth';
+  const KAKAO_AUTH_URI = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+
+  const parseAuthCode = async (url) => {
+    const exp = 'code='; //url에 붙어 날라오는 인가코드는 code=뒤부터 parse하여 get
+    const startIndex = url.indexOf(exp); //url에서 "code="으로 시작하는 index를 찾지 못하면 -1반환
+    if (startIndex !== -1) {
+      const authCode = url.substring(startIndex + exp.length);
+      console.log('access code :: ' + authCode);
+      console.log(url);
+
+      // await axios
+      //   .post('본인 url', {
+      //     params: {
+      //       code: authCode,
+      //     },
+      //   })
+      //   .then((res) => AsyncStorage.setItem('userNumber', JSON.stringify(res['data']['userId'])));
+
+      // navigate('Home', { screen: 'Home' });
+    }
+  };
+
   return (
-    <View>
-      <Text>마이페이지</Text>
+    <View style={{ flex: 1 }}>
+      <Text>로그인 화면</Text>
+      <WebView
+        originWhitelist={['*']}
+        scalesPageToFit={false}
+        style={{ marginTop: 30 }}
+        source={{
+          uri: KAKAO_AUTH_URI,
+        }}
+        injectedJavaScript={runFirst}
+        javaScriptEnabled={true}
+        onMessage={(event) => {
+          parseAuthCode(event.nativeEvent['url']);
+        }}
+
+        // onMessage ... :: webview에서 온 데이터를 event handler로 감지하여 parseAuthCood로 전달
+      />
     </View>
   );
-}
+};
 
 export default MypageScreen;
