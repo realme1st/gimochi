5da351485dda27c335ee07605bf87df8458d1762
SeongMin Lee <62661142+realme1st@users.noreply.github.com>
2022-11-01 16:33:44 +0900

    :bug: fix : apllication 수정 application 수정

--- backend/src/main/java/com/ssafy/api/dto/ChallengeRewardReqDto.java
+++ backend/src/main/java/com/ssafy/api/dto/ChallengeRewardReqDto.java
@@ -1,10 +1,13 @@
 package com.ssafy.api.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class ChallengeRewardReqDto {
 
+    @ApiModelProperty(name="챌린지 id", example="1")
     private Long challengeId;
+    @ApiModelProperty(name="챌린지 타입 (0이면 포인트 1이면 기프티콘)", example="1")
     private Long challengeRewardType;
 }
