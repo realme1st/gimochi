d4bbe12ac2fb0e0d9e30f6a0d56b01552fec5480
KimDongWook <kdw508@naver.com>
2022-11-01 10:46:12 +0900

    :sparkles: feat : notification 기능 추가 파이어베이스 알림 구현

--- backend/src/main/java/com/ssafy/api/controller/NotificationController.java
+++ backend/src/main/java/com/ssafy/api/controller/NotificationController.java
@@ -7,10 +7,10 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Api(value = "푸시 알림 API", tags = {"Notification"})
 @RestController
@@ -30,5 +30,21 @@ public class NotificationController {
             @RequestHeader(value = "AccessToken") String accessToken, @RequestHeader(value = "FirebaseToken") String firebaseToken) {
         User user = notificationService.saveToken(accessToken,firebaseToken);
         return user;
+    }
+
+    //cd6e7_ZHRRewNpQK3yndXN:APA91bGuDmUdfG4mgkFsvc-m3T-JJ2U1h4oLlNJAZjCfiq6SYwX1EHBRZmbi6Sqnoq_tv8H5HnrwXmhbB8ZbgBpkjqLOb6L-c26J70THIEW1iKuz-H_l0nUfaEk9yOABn5CqZqbguf2u
+    @PostMapping("/message")
+    @ApiOperation(value = "메세지 보내기", notes = "test")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공"),
+            @ApiResponse(code = 401, message = "발급 실패"),
+            @ApiResponse(code = 500, message = "서버 오류")
+    })
+    public User registFirebaseToken(
+            @RequestParam String token) {
+        List<String> tokens = new ArrayList<>();
+        tokens.add(token);
+        notificationService.sendByTokenList(tokens);
+        return null;
     }
 }
