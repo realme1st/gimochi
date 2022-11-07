bcad918ea44111f94b06c9c615f1f61c0d38b294
전민재 <minjae77777@naver.com>
2022-11-07 09:49:55 +0900

    :bug: fix : hotfix delete node_modules

--- FrontEnd/Gimochi/src/screen/challenge/ChallengeMainScreen.tsx
+++ FrontEnd/Gimochi/src/screen/challenge/ChallengeMainScreen.tsx
@@ -45,12 +45,12 @@ function ChallengeMainScreen({ navigation }) {
       </Tab>
       <TabView value={index} onChange={setIndex} animationType='spring'>
         <TabView.Item style={{ backgroundColor: 'red', width: '100%' }}>
-          <Text h1>Recent</Text> <Text h1>Recent</Text> <Text h1>Recent</Text> <Text h1>Recent</Text>{' '}
-          <Text h1>Recent</Text> <Text h1>Recent</Text> <Text h1>Recent</Text> <Text h1>Recent</Text>{' '}
-          <Text h1>Recent</Text> <Text h1>Recent</Text>
+          <Text h1>Recent</Text>
         </TabView.Item>
         <TabView.Item style={{ backgroundColor: 'blue', width: '100%' }}>
-          <Text h1>Favorite</Text>
+          {/* <ScrollView> */}
+          <Text h1>Favorite</Text> <Text h1>Favorite</Text>
+          {/* </ScrollView> */}
         </TabView.Item>
       </TabView>
       {/* </ScrollView> */}
