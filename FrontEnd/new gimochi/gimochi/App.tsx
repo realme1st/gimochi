05e3c4c6061c77aa1225747526dc48f5c8ea7766
SH <shhh1006@daum.net>
2022-11-12 00:10:56 +0900

    :hammer: chore : Rebuild RN version 0.68.2 - 버전 잘못 빌드한거 지우고 다시 빌드

--- FrontEnd/new gimochi/gimochi/App.tsx
+++ FrontEnd/new gimochi/gimochi/App.tsx
@@ -1,23 +1,26 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /**
+
  * Sample React Native App
+
  * https://github.com/facebook/react-native
+
  *
+
  * Generated with the TypeScript template
+
  * https://github.com/react-native-community/react-native-template-typescript
+
  *
+
  * @format
+
  */
 
-import React, {type PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import React from 'react';
+
+import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
 
 import {
   Colors,
@@ -27,30 +30,34 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const Section: React.FC<
-  PropsWithChildren<{
-    title: string;
-  }>
-> = ({children, title}) => {
+const Section: React.FC<{
+  title: string;
+}> = ({ children, title }) => {
   const isDarkMode = useColorScheme() === 'dark';
+
   return (
     <View style={styles.sectionContainer}>
       <Text
         style={[
           styles.sectionTitle,
+
           {
             color: isDarkMode ? Colors.white : Colors.black,
           },
-        ]}>
+        ]}
+      >
         {title}
       </Text>
+
       <Text
         style={[
           styles.sectionDescription,
+
           {
             color: isDarkMode ? Colors.light : Colors.dark,
           },
-        ]}>
+        ]}
+      >
         {children}
       </Text>
     </View>
@@ -66,31 +73,31 @@ const App = () => {
 
   return (
     <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+
+      <ScrollView contentInsetAdjustmentBehavior='automatic' style={backgroundStyle}>
         <Header />
+
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
+          }}
+        >
+          <Section title='Step One'>
+            Edit <Text style={styles.highlight}>App.tsx</Text> to change this screen and then come back to see
+            your edits.
           </Section>
-          <Section title="See Your Changes">
+
+          <Section title='See Your Changes'>
             <ReloadInstructions />
           </Section>
-          <Section title="Debug">
+
+          <Section title='Debug'>
             <DebugInstructions />
           </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
+
+          <Section title='Learn More'>Read the docs to discover what to do next:</Section>
+
           <LearnMoreLinks />
         </View>
       </ScrollView>
@@ -101,17 +108,24 @@ const App = () => {
 const styles = StyleSheet.create({
   sectionContainer: {
     marginTop: 32,
+
     paddingHorizontal: 24,
   },
+
   sectionTitle: {
     fontSize: 24,
+
     fontWeight: '600',
   },
+
   sectionDescription: {
     marginTop: 8,
+
     fontSize: 18,
+
     fontWeight: '400',
   },
+
   highlight: {
     fontWeight: '700',
   },
