05444768edff049f06acb56c8b67ded40855a01d
KimDongWook <kdw508@naver.com>
2022-11-15 14:23:44 +0900

    :hammer: chore : 카카오 api 수정 api 수정

--- backend/src/main/java/com/ssafy/api/dto/TokenInforamtion.java
+++ backend/src/main/java/com/ssafy/api/dto/TokenInforamtion.java
@@ -1,19 +1,15 @@
 package com.ssafy.api.dto;
 
-import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class TokenInforamtion {
 
     private Long id;
     private int expires_in;
     private int app_id;
-
-    @Builder
-    public TokenInforamtion(Long id, int expires_in, int app_id) {
-        this.id = id;
-        this.expires_in = expires_in;
-        this.app_id = app_id;
-    }
+    private int expiresInMillis;
+    private int appId;
 }
