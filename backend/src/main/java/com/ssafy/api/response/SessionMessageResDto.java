fa73dbbb8d24326ecce1b970e798492ec68786ee
김다은 <vkfksqlcgkxm@naver.com>
2022-11-17 13:31:01 +0900

    :sparkles: feat : sessionMessage 반환갑 기프티콘 정보 추가 sessionMessage 반환갑 기프티콘 정보 추가 - gifticonStore

--- backend/src/main/java/com/ssafy/api/response/SessionMessageResDto.java
+++ backend/src/main/java/com/ssafy/api/response/SessionMessageResDto.java
@@ -1,6 +1,7 @@
 package com.ssafy.api.response;
 
 import com.ssafy.api.request.SessionMessageReqDto;
+import com.ssafy.db.entity.Gifticon;
 import com.ssafy.db.entity.SessionMessage;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -18,15 +19,18 @@ public class SessionMessageResDto {
     private String nickname;
     private String field;
     private LocalDate expireTime;
+
+    private String gifticonStore;
     private int messageType;
 
     @Builder
-    public SessionMessageResDto(Long sessionMessageId, String nickname, String field, LocalDate expireTime, int messageType) {
+    public SessionMessageResDto(Long sessionMessageId, String nickname, String field, LocalDate expireTime, int messageType, String gifticonStore) {
         this.sessionMessageId = sessionMessageId;
         this.nickname = nickname;
         this.field = field;
         this.expireTime = expireTime;
         this.messageType = messageType;
+        this.gifticonStore = gifticonStore;
     }
 
     public static SessionMessage toDto(SessionMessageReqDto reqDto){
@@ -45,6 +49,7 @@ public class SessionMessageResDto {
                 .field(sessionMessage.getField())
                 .expireTime(sessionMessage.getExpireTime())
                 .messageType(sessionMessage.getMessageType())
+                .gifticonStore(sessionMessage.getGifticon().getGifticonStore())
                 .build();
     }
 
@@ -58,6 +63,7 @@ public class SessionMessageResDto {
                     .nickname(m.getNickname())
                     .field(m.getField())
                     .expireTime(m.getExpireTime())
+                            .gifticonStore(m.getGifticon().getGifticonStore())
                     .build());
         }
         return sessionMessageResDtoList;
